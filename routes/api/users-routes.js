f15ee26ac0be4c82d78162a0f354c564e1871d5c
Niraj Limbu <nlimbu@yahoo.com>
2022-03-09 00:51:59 -0500

    create user routes

---
+++ routes/api/users-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers');
+
+// Set up GET all and POST at /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+//  Set up GET one, PUT, and DELETE at /api/users/:id
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+// POST and DELETE friends
+router.route('/:id/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
